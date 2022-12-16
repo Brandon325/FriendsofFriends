67a4a8b5b3480211fc92cfc234e4159a7d43a90b
Brandon325 <46274399+Brandon325@users.noreply.github.com>
2022-12-15 19:02:40 -0500

    Add files via upload
    Final Version Commit

--- main.rs
+++ main.rs
@@ -8,7 +8,6 @@ use crate::bfs::breadth_first_search;
 use crate::least_similar_nodes::find_least_similar_nodes;
 use crate::most_similar_nodes::find_most_similar_nodes;
 
-
 fn main() {
     // Read the input file
     let input = read_file("facebook_combined.txt");
@@ -23,4 +22,36 @@ fn main() {
     // Find the most similar pair of nodes
     let most_similar_nodes = find_most_similar_nodes(adjacency_list2);
     println!("The most similar nodes are: {:?}", most_similar_nodes);
+
+}
+
+//Tests
+#[test]
+fn test_breadth_first_search() {
+    let input = vec![(1, 2), (1, 3), (1, 4), (2, 5), (3, 5), (4, 5)];
+    let expected_output = vec![(1, vec![2, 3, 4]), (2, vec![5]), (3, vec![5]), (4, vec![5]), (5, vec![])];
+    assert_eq!(breadth_first_search(input), expected_output);
+
+    let input = vec![(1, 2), (2, 3), (3, 4), (4, 5), (5, 6)];
+    let expected_output = vec![(1, vec![2]), (2, vec![3]), (3, vec![4]), (4, vec![5]), (5, vec![6]), (6, vec![])];
+    assert_eq!(breadth_first_search(input), expected_output);
+
+    let input = vec![(1, 2), (1, 3), (1, 4), (1, 5), (1, 6)];
+    let expected_output = vec![(1, vec![2, 3, 4, 5, 6]), (2, vec![]), (3, vec![]), (4, vec![]), (5, vec![]), (6, vec![])];
+    assert_eq!(breadth_first_search(input), expected_output);
+}
+
+#[test]
+fn test_find_most_similar_nodes() {
+    let input = vec![(1, vec![2, 3, 4]), (2, vec![5]), (3, vec![5]), (4, vec![5]), (5, vec![2,3,4])];
+    let expected_output = (1, 5);
+    assert_eq!(find_most_similar_nodes(input), expected_output);
+
+    let input = vec![(1, vec![2]), (2, vec![3]), (3, vec![4]), (4, vec![5]), (5, vec![6]), (6, vec![2])];
+    let expected_output = (1, 6);
+    assert_eq!(find_most_similar_nodes(input), expected_output);
+
+    let input = vec![(1, vec![2, 3, 4, 5, 6]), (2, vec![3,4,5]), (3, vec![]), (4, vec![]), (5, vec![]), (6, vec![])];
+    let expected_output = (1, 2);
+    assert_eq!(find_most_similar_nodes(input), expected_output);
 }
